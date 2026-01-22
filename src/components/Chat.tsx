09eaab28b5d4d5e104a9d0f56d76899f9cc51899
Rodrigo Yuji Seto Soma <rodrigo.y.seto@gmail.com>
2026-01-22 17:11:25 +0900

    feat: add NameInput component for user name entry

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -1,3 +1,11 @@
+import { useUser } from "@/contexts/UserContext";
+import { NameInput } from "./NameInput";
+
 export const Chat = () => {
+  const userCtx = useUser();
+
+  if (!userCtx) return null;
+  if (!userCtx.user) return <NameInput />;
+
   return <div className="border border-white/30 rounded-md">asd</div>;
 };
